fe888a5d959174f4a35b22ba945e65a72103e464
shinyeji28 <xntm10@naver.com>
2023-05-11 18:18:18 +0900

    no message

--- swea_1244_최대상금.py
+++ swea_1244_최대상금.py
@@ -1,64 +1,48 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// float b, c;
-// double d, e, f;
-// char g;
-// char var[256];
-// long long AB;
-// scanf("%d", &a);                      // int 변수 1개 입력받는 예제
-// scanf("%f %f", &b, &c);               // float 변수 2개 입력받는 예제
-// scanf("%lf %lf %lf", &d, &e, &f);     // double 변수 3개 입력받는 예제
-// scanf("%c", &g);                      // char 변수 1개 입력받는 예제
-// scanf("%s", &var);                    // 문자열 1개 입력받는 예제
-// scanf("%lld", &AB);                   // long long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;
-// float b = 1.0, c = 2.0;
-// double d = 3.0, e = 0.0; f = 1.0;
-// char g = 'b';
-// char var[256] = "ABCDEFG";
-// long long AB = 12345678901234567L;
-// printf("%d", a);                      // int 변수 1개 출력하는 예제
-// printf("%f %f", b, c);                // float 변수 2개 출력하는 예제
-// printf("%lf %lf %lf", d, e, f);       // double 변수 3개 출력하는 예제
-// printf("%c", g);                      // char 변수 1개 출력하는 예제
-// printf("%s", var);                    // 문자열 1개 출력하는 예제
-// printf("%lld", AB);                   // long long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-#include <stdio.h>
-int main(void)
-{
-	int test_case;
-	int T;
-	/* 아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
-	앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-	여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-	freopen 함수를 이용하면 이후 scanf 를 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-	따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
-	단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
-	*/
-	// freopen("input.txt", "r", stdin);
-	/* 아래 코드를 수행하지 않으면 여러분의 프로그램이 제한 시간 초과로 강제 종료 되었을 때,
-	출력한 내용이 실제 표준 출력에 기록되지 않을 수 있습니다.
-	따라서 안전을 위해 반드시 setbuf(stdout, NULL); 을 수행하시기 바랍니다.
-	*/
-	setbuf(stdout, NULL);
-	scanf("%d", &T);
-	/*
-	여러 개의 테스트 케이스를 처리하기 위한 부분입니다.
-	*/
-	for (test_case = 1; test_case <= T; ++test_case)
-	{
-		/////////////////////////////////////////////////////////////////////////////////////////////
-		/*
-		이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-		*/
-		/////////////////////////////////////////////////////////////////////////////////////////////
+# 1. dfs를 사용해서 완전 탐색
+# 2. max값을 미리 찾은 후 dfs에서 모두 사용한 횟수 레벨에서 현재 값과 비교하며 같으면 탈출 - 시간 줄이기
+# 3. max값과 같은 값을 찾지 못하면 저장된 값 중 큰 값을 출력
 
-	}
-	return 0; //정상종료시 반드시 0을 리턴해야 합니다.
-}
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+def dfs(numbers,L):
+    global num, result, maxNum,isFind,same
+    n = int(''.join(numbers))
+    if isFind:
+        return
+    if maxNum == n:
+        if L%2==1 and not same:
+            numbers[-2], numbers[-1] = numbers[-1], numbers[-2]
+            result = int(''.join(numbers))
+        else:
+            result = maxNum
+        isFind = True
+        return
+
+    if L == 0:
+        if result < n:
+            result = n
+        return
+
+    for i in range(len(numbers)-1):
+        for j in range(i+1,len(numbers)):
+            if numbers[i]<numbers[j]:
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+                dfs(numbers,L-1)
+                numbers[i], numbers[j] = numbers[j], numbers[i]
+
+for test_case in range(1, T + 1):
+    result = 0
+    isFind = False
+    num, L = input().split()
+    _sorted = sorted(num,reverse=True)
+    maxNum = int(''.join(_sorted))
+    same = False
+    for i in range(len(_sorted)-1):
+        for j in range(i+1,len(_sorted)):
+            if _sorted[i]==_sorted[j]:
+                same =True
+                break
+        if same:
+            break
+    dfs(list(num),int(L))
+    print('#{} {}'.format(test_case,result))
