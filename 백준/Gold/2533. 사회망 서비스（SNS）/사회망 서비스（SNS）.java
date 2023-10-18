1ca61505aac8f900faafca0be9f53b35d47f530c
shinyeji28 <71514775+shinyeji28@users.noreply.github.com>
2023-10-18 16:01:42 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1936 ms, Memory: 404424 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.java
@@ -1,16 +1,21 @@
 /**
  * 조건>
- * 우수 마을로 선정된 주민 총합이 최대
- * 인접한 마을 모두 우수마을로 선정하는 것은 불가
- * 선정되지 못한 마을은 우수마을에 인접해야 함
- * 
- * 구현 방법>
- * 우수마을 - 미선정 - 우수마을 이런식으로 들쑥날쑥 했을 때 최대 주민 수 합
- * 트리 + dp
- * dp : idx를 정점번호로 하는 자식노드부터 해당 노드까지의 선정/미선정을 종합한 최대 주민수 합
- * dp_우수마을 선정[n] = dp_우수마을 미선정[n의 자식] + n정점의 주민 수
+ * 얼리 아답터가 아닌 사람들은 자신의 모든 친구들이 얼리 아답터일 때만 이 아이디어를 받아들인다.
+ * 트리
  * 출력 >
- * Max(dp_우수마을 선정[0], dp_우수마을 미선정[0])
+ * 모든 개인이 최소 아이디어를 수용하기 위해 필요한 최소 얼리 어답터의 수 구하기
+ * 
+ * 구현방법>
+ * DFS + dp
+ * n이 얼리어답터일 때 -> 자식은 얼리어답터여도 되고 아니여도 된다 - dp[0][rootNode] += 1 + Math(dp[0][childNode], dp[1][childNode]);
+ * n이 얼리어답터가 아닐 때 -> 자식은 얼리어답터여야 한다.     - dp[1][rootNode] += dp[0][childNode];
+ * 
+ * dp : n을 root로 하는 얼리어답터 수
+ * dp_얼리어답터 선정[n] : 정점 n이 얼리어답터일 때
+ * dp_얼리어답터 미선정[n] : 정점 n이 얼리어답터가 아닐 때
+ * 
+ * 
+ *
  */
 
 import java.util.*;
@@ -37,25 +42,17 @@ public class Main {
 			graph[from].add(to);
 			graph[to].add(from);
 		}
-//		print();
-		
-		// dp[0][n] : n을 우수마을로 선정
-		// dp[1][n] : n을 우수마을로 미선정
+
 		dp = new int[2][N+1];
 		dfs(1);
-//		System.out.println(Arrays.toString(dp[0]));
-//		System.out.println(Arrays.toString(dp[1]));
+
 		System.out.println(Math.min(dp[0][1],dp[1][1]));
 	}
-	private static void print() {
-		for (int i = 1; i < graph.length; i++) {
-			System.out.println(graph[i].toString());
-		}
-	}
+
 	private static void dfs(int v) {
 		visited[v] = true;
 		
-		dp[0][v] += 1;  // 우수마을 선정
+		dp[0][v] += 1;  // 얼리어답터 선정
 		
 		boolean flag = true;
 		for (Integer node: graph[v]) {
@@ -68,7 +65,7 @@ public class Main {
 
 		
 		if(flag) {  // 리프노드
-			dp[0][v] = 1;  // 우수마을 선정
+			dp[0][v] = 1;  // 얼리어다터 선정 선정
 		}
 		
 	}
