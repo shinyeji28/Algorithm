e2a2a44c05f237049d5cc323ebe24d7a19189fac
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-06-09 16:26:25 +0900

    [Gold III] Title: 소문난 칠공주, Time: 320 ms, Memory: 149660 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,106 +1,91 @@
-/**
- * 칠공주 조건
- *   7명으로 구성
- *   사방에 인접
- *   '이다솜파'학생이 4명이상 포함
- * 입력>
- * S - 이다솜파
- * Y - 임도연파
- *   
- * 출력>
- * 칠공주를 결성할 수 있는 모든 경우의 수
- * 
- * 구현방법>
- * 1. 7명 뽑기 - 조합 (2차원 배열 -> 1차원으로 만들어 7명 뽑기)
- * 2. 뽑은 7명이 칠공주를 만족하는지 - DFS탐색
- *    - 인접하는가
- *    - 이다솜파가 4명 이상인가
- *    
- *    
- * 
- */
-
+/*
+* 소문난 칠공주
+* 각 그룹은 7명의 여학생들
+* 칠공주는 가로세로 인접
+* 이다솜파가 4명이상 존재
+*
+*
+* 방법 >
+* 1)
+* 1. 인접한 7개 bfs를 통해 확인
+* 2. 남은개수 + S가 < 4 탐색 종료
+* -> 먼저 인접한 것을 찾으면 방문했던 경우를 또 방문하는 경우가 너무 많아짐
+* 2)
+* 1. 2차원 행렬을 1차원으로 만들기 (0~24까지를 x=i/5, y=i%5) -> 반대는 (x * size + y)
+* 2. 7개를 뽑기
+* 3. 인접한지 확인 (백트래킹)
+* 4. 남은개수 + S가 < 4 탐색 종료
+*
+* 시간 복잡도 >
+*     O(25C7 *7)
+*
+*
+* */
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
+public class Main {
+    final static int size = 5;
+    static char[][] map = new char[size][size];
+    static boolean[] selected = new boolean[size*size];
+    static int[][] pos = new int[7][2];
+    static int[] dx = new int[]{0,0,1,-1};
+    static int[] dy = new int[]{1,-1,0,0};
+    static int result;
+    public static void main(String[] args) throws IOException {
 
-public class Main  {
-	static final int SIZE = 5;
-	static char[][] map = new char[SIZE][SIZE];
-	static int[][] pick;
-	static int result;
-	static int[] dx = new int[] {0,0,-1,1};
-	static int[] dy = new int[] {-1,1,0,0};
-	static int adj;
-	static boolean[][] visited;
-	static boolean isPrincess[][];
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		pick = new int[7][2];
-		
-		isPrincess= new boolean[SIZE][SIZE];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i=0;i<size;i++){
+            String st = br.readLine();                          // readLine() : String으로 받음
+            map[i] = st.toCharArray();                          // toCharArray() : String을 Array의 char으로 변경
+        }
+        combination(0, 0, 0);
+        System.out.println(result);
+    }
+    public static void combination(int start, int depth, int sCount){
 
-		for (int i = 0; i < SIZE; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			for (int j = 0; j < SIZE; j++) {
-				map[i][j] = str.charAt(j);
-			}
-		}
-		
-		combination(0,0);
-		
-		System.out.println(result);
-	}
-	private static void combination(int start, int depth) {
-		if(depth==7) {
-			
-
-			
-			// 인접하면서 이다솜파가 4명 이상인지 확인하기
-			int cnt = 0;
-
-			for (int i = 0; i < 7; i++) {
-				if(map[pick[i][0]][pick[i][1]] == 'S')cnt++;
-				isPrincess[pick[i][0]][pick[i][1]] = true;
-				
-			}
-			
-			if(cnt>=4) {
-				visited= new boolean[SIZE][SIZE];
-				adj = 0;
-				check(pick[0][0],pick[0][1]);
-
-				if(adj==7) {
-					result++;
-				}
-			}
-			isPrincess = new boolean[SIZE][SIZE];
-			return;
-		}
-		for (int i = start; i < 25; i++) {
-			// 뽑기
-			pick[depth][0] = i/SIZE;
-			pick[depth][1] = i%SIZE;
-			combination(i+1,depth+1);
-		}
-	}
-	private static void check(int cx, int cy) {
-		adj++;
-		visited[cx][cy] = true;
-
-		
-		if(adj==7)return;
-		
-		for (int i = 0; i < dx.length; i++) {
-			int nx = dx[i] + cx;
-			int ny = dy[i] + cy;
-			
-			if(nx<0||ny<0||nx>=SIZE||ny>=SIZE)continue;
-			if(visited[nx][ny])continue;
-			if(!isPrincess[nx][ny])continue;
-			
-			check(nx,ny);
-		}
-	}
-
+        // 'S'>=4이상 될 수 없으면 백트래킹
+        if(sCount + (7-depth) < 4)return;
+        // 인접한지 체크
+        if(depth == 7){
+            if(checkAdjacency()) {
+                result++;
+            }
+            return;
+        }
+        // 7개 뽑기
+        for(int i=start;i<size*size;i++){
+            int x = i/size;                                        // 2차원 행렬을 1차원으로 변경 x: i / size, y : i % size
+            int y = i%size;
+            selected[i] = true;
+            pos[depth][0] = x;
+            pos[depth][1] = y;
+            if(map[x][y] == 'S'){
+                combination(i+1, depth+1,sCount+1);
+            }else{
+                combination(i+1, depth+1,sCount);
+            }
+            selected[i] = false;
+        }
+    }
+    public static boolean checkAdjacency(){
+        // bfs로 인접한지 확인
+        boolean[][] visited = new boolean[size][size];
+        Queue<int[]> q = new ArrayDeque<>();
+        int cnt=1;
+        q.add(new int[]{pos[0][0],pos[0][1]});
+        visited[pos[0][0]][pos[0][1]] = true;
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0], y = cur[1];
+            for(int d = 0;d < dx.length;d++){
+                int nx = x + dx[d], ny = y + dy[d];
+                if(nx<0 || ny<0 || nx>=size || ny>=size || visited[nx][ny] || !selected[nx * size + ny])continue;     // x,y로 일차원 번호 얻는 방법은 x * size + y
+                q.offer(new int[]{nx, ny});
+                visited[nx][ny] = true;
+                cnt++;
+            }
+        }
+        return cnt==7;  // 주의 while문 안에서 조건을 체크하면 isEmpty로 조건을 수행할 수 없는 경우가 발생
+    }
 }
