a5fda37b2b8cfd09b852962f414dc003d5609986
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-06-10 16:16:49 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,66 +1,50 @@
-/**
- * N개의 물건 중 
- * 무게 W와 가치 V를 가지는데 
- * 최대 K만큼의 무게만 넣을 수 있는 배낭에 최대  V를 넣어라
- * 
- * 구현 방법
- * DP[고려할 물건들 (1~i)][부분 배낭] = 최대 가치
- * 
- * 점화식
- * 무게가 0 && 부분배낭이 담을 수 있는 무게가 0 : 가치 0
- * 부분 배낭에 담을 수 있는 무게 < 현재 고려하는 물건의 무게 : 이전 최적해를 저장
- *                      >         : max(담는다: 현재 고려하는 물건의 가치 + 부분배낭에 담을 수 있는무게 - 현물무
- *                                  ,담지 않는다 : 이전 최적해 저장)
- * 									
- * 
- */
+/*
+*  V(가치)의 최대값 구하기
+*
+* W(무게) <= K (배낭)
+*
+* 방법 >
+* 무게가 작은 것부터 넣거나 가치가 큰 것부터 넣으면 안됨
+*
+* 1) 완탐
+* 모든 경우를 다 고려해야 함
+* 시간 복잡도 : O(N^N) = 100^100 - 시간 초과
+* 2) DP
+* 아이디어 : 물건 하나를 담을 때, 1~k무게를 담는 가방의 최대 가치
+* dp[i] : i라는 무게를 담는 가방에 대한 최대의 가치
+* (w,v)의 물건을 가방에 들어갈 경우 k 무게의 가방에 들어갈 가치 = v + dp[k-w]
 
-import java.util.*;
+*
+* */
+
 import java.io.*;
-
+import java.util.*;
 public class Main {
-	public static class Item{
-		int w;
-		int v;
-		public Item (int w,int v) {
-			this.w = w;
-			this.v = v;
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int finalWeight = Integer.parseInt(st.nextToken());
-		int[][] dp = new int[n+1][finalWeight+1];
-		
-		List<Item> items = new ArrayList<>();
-		items.add(new Item(0,0));
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			items.add(new Item(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-		}
-		
-		// dp테이블 만들기
-		for (int i = 0; i < dp.length; i++) {
-			dp[i][0] = dp[0][i] = 0;
-		}
-		
-		for (int i = 1; i < dp.length; i++) {
-			for (int j = 1; j < dp[0].length; j++) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-				if(j < items.get(i).w) {
-					dp[i][j] = dp[i-1][j];
-				}else {
+        Queue<int[]> goods = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            goods.offer(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
 
-					dp[i][j] = Math.max(items.get(i).v + dp[i-1][j - items.get(i).w], dp[i-1][j]);
-				}
-	
-			}
-		}
-		System.out.println(dp[n][finalWeight]);
-		
-	}
-	
+        int[] dp = new int[k+1];
+
+        // 물건을 하나씩 가방에 넣기
+        A : for(int i=0;i<n;i++){
+            int[] item = goods.poll();
+            int w = item[0];
+            int v = item[1];
+
+            for(int j=k;j>=1;j--){   // 임시 가방(무게가 1~k)에 해당 물건을 넣을 때 각 임시 가방의 최대 가치
+                if(w>j) continue A;   // 가방의 무게보다 물건이 무거우면 더 이상 탐색할 필요 없음
+                dp[j] = Math.max(dp[j], dp[j-w] + v);
+            }
+        }
+        System.out.println(dp[k]);
+    }
 }
