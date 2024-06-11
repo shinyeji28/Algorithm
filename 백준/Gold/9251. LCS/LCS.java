ebb5de7c80f0d536b01aa50cbf84f453f9943a70
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-06-11 17:38:19 +0900

    [Gold V] Title: LCS, Time: 96 ms, Memory: 15940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,41 +1,34 @@
+/**
+ * 방법 >
+ * 문자열을 2차원 dp로 풀기
+ * 아이디어 : 나를 포함할 것과 나를 포함하지 않는 것중 더 큰 것을 dp에 저장
+ * 비교 문자가 같으면 : dp[i][j] = max(dp[i-1][j-1]+1, dp[i-1][j])
+ * 비교 문자가 다르면 : dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+ */
 import java.util.*;
 import java.io.*;
-/**
- * 구현 방법>
- * lcs -> 2차원 dp배열
- * 점화식
- *  i==j? i를 부분 수열로 선택한다 -	dp[i][j] = dp[i-1][j-1] +1
- *  else i를 부분 수열로 선택하지 않는다 - Math.max(dp[i-1][j-1], dp[i][j-1])
- *  
- * dp[i]][j] 정의 :문자열2[1] ~ 문자열2[j]과  문자열1[1] ~ 문자열1[i]까지의 최장 공통 부분 수열의 개수  
- */
 public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        char[] alph1 = str.toCharArray();
+        st = new StringTokenizer(br.readLine());
+        str = st.nextToken();
+        char[] alph2 = str.toCharArray();
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		String str1 = "0" + st.nextToken();
-		st = new StringTokenizer(br.readLine());
-		String str2 = "0" + st.nextToken();
-		
-		int dp[][] = new int[str1.length()+1][str2.length()+1];
-		for(int i=0;i<str1.length();i++) {
-			for(int j=0;j<str2.length();j++) {
-				if(i==0 || j==0) {
-					dp[i][j] = 0;
-					continue;
-				}
-				if(str1.charAt(i)==str2.charAt(j)) {
-					dp[i][j] = dp[i-1][j-1] + 1;
-				}else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-				}
-			}
-
-		}
-
-		System.out.println(dp[str1.length()-1][str2.length()-1]);
-
-	}
-
+        int len1 = alph1.length;
+        int len2 = alph2.length;
+        int[][] dp = new int[len1+1][len2+1];
+        for(int i=1;i<=len1;i++){
+            for(int j=1;j<=len2;j++){
+                if(alph1[i-1]==alph2[j-1]){
+                    dp[i][j] = Math.max(dp[i-1][j-1]+1, dp[i-1][j]);
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        System.out.println(dp[len1][len2]);
+    }
 }
