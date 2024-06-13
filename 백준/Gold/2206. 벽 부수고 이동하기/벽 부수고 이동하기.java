55570765a35dc874ceb49c26687aaed270b56548
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-06-13 14:51:29 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 540 ms, Memory: 135868 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,81 +1,68 @@
+/*
+* 벽을 0or1번 뚫을 수 있을 때, 이때 최단 거리
+* dp, 방문 배열에 벽을 뚫었는지 기록
+* visited[0][i][j] : 벽을 안뚫은 경우 / visited[1][i][j] : 벽을 뚫은 경우
+* 최단거리는 다익스트라로 풀이 (가장 먼저 nn에 도착한 것이 최단거리)
+* dp[i][j] = min(벽을 뚫은 경우, 벽을 뚫지 않는 경우)
+* */
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-
+import java.util.*;
+import java.io.*;
 public class Main {
+    static char[][] map;
+    static int n,m;
+    static int[] dx = new int[]{0,0,1,-1};
+    static int[] dy = new int[]{1,-1,0,0};
+    public static void main(String[] agrs)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new char[n][m];
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            map[i] = str.toCharArray();
+        }
+        System.out.println(bfs());
+    }
+    public static int bfs(){
+        Queue<int[]> pq = new LinkedList<>();
+        pq.add(new int[]{0,0,1,0});
+        boolean[][][] visited = new boolean[2][n][m];
+        visited[0][0][0] = true;
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int x = cur[0];
+            int y = cur[1];
+            int c = cur[2];
+            int isWall = cur[3];
+            if(x == n-1 && y == m-1){
+                return c;
+            }
+            for(int d=0;d<dx.length;d++){
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if(nx<0|| ny<0|| nx>=n||ny>=m)continue;
 
-	public static class Node{
-		int x,y,dist;
-		boolean isWallBreak;
-		
-		public Node(int x, int y,int d, boolean w) {
-			this.x = x;
-			this.y = y;
-			this.dist = d;
-			this.isWallBreak = w;
-		}
-	}
-	public static void main(String[] args) throws Exception{
+                if(isWall==1){  // 벽을 부순적이 있음
+                    if(!visited[1][nx][ny] && map[nx][ny] == '0'){
+                        pq.offer(new int[]{nx,ny,c+1,1});
+                        visited[1][nx][ny] = true;
+                    }
+                }
+                else{  // 벽을 부순적이 없음
+                    if (map[nx][ny] == '1') {   // 벽을 부숨
+                        pq.offer(new int[]{nx, ny, c + 1, 1});
+                        visited[1][nx][ny] = true;
+                    }else if(!visited[0][nx][ny]){
+                        pq.offer(new int[]{nx,ny,c+1,0});
+                        visited[0][nx][ny] = true;
+                    }
+                }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String[] str = br.readLine().split(" ");
-		int N = Integer.parseInt(str[0]);
-		int M = Integer.parseInt(str[1]);
-		
-		char[][] map = new char[N][M];
-		for(int i=0;i<N;i++) {
-			String temp = br.readLine();
-			for(int j=0;j<M;j++) {
-				map[i][j] = temp.charAt(j);
-			}
-		}
-		map[0][0] = 1;
-		
-		boolean[][][] visited = new boolean[N][M][2];
-		Node cur;
-		int x,y;
-		int[] dx = {0,0,1,-1};
-		int[] dy = {1,-1,0,0};	
-		Queue<Node>q = new ArrayDeque<Node>();             // [x, y, distance, isWallbreak]
-		q.offer(new Node (0,0,0,false));
-	
-		while(!q.isEmpty()) {
-			cur = q.poll();
-		
-			if(cur.x == N-1 && cur.y == M-1) {
-				System.out.println(cur.dist+1);
-				return;
-			}
-
-			
-			for(int d=0;d<dx.length;d++) {
-				x = cur.x + dx[d];
-				y = cur.y + dy[d];
-				if(x<0 || y<0 || x>=N || y>=M) continue;
-				if(map[x][y] == '1') {
-					if(cur.isWallBreak==false) {
-						visited[x][y][1] = true;
-						q.offer(new Node (x,y,cur.dist+1,true));
-					}
-				}else {
-					if(cur.isWallBreak==false && visited[x][y][0]==false) { // 현재까지 벽을 부순적 없고 방문하지 않았으면
-						 visited[x][y][0] = true;
-						 q.offer(new Node (x,y,cur.dist+1,false));
-					}
-					else if(cur.isWallBreak==true && visited[x][y][1]==false) {  // 벽을 부순적이 있고 방문하지 않았으면 
-						visited[x][y][1] = true;
-						 q.offer(new Node (x,y,cur.dist+1,true));
-					}
-					
-				}
-			}
-		}
-		
-		System.out.println("-1");
-		
-	}
-
+            }
+        }
+        return -1;
+    }
 }
