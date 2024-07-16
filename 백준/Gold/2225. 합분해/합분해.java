e792ed7e279fa96f455623468c080c6bc8d11f38
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-07-16 13:48:13 +0900

    [Gold V] Title: 합분해, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,28 +1,29 @@
-import java.util.Scanner;
-
-public class Main {
-    private static final int MOD = 1_000_000_000;
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
-
-        int[][] dp = new int[K+1][N+1];
-
-        // 초기화: 1개의 숫자로 j를 만드는 방법은 1가지
-        for (int j = 0; j <= N; j++) {
-            dp[1][j] = 1;
-        }
-
-        // DP 점화식 적용
-        for (int i = 2; i <= K; i++) {
-            for (int j = 0; j <= N; j++) {
-                dp[i][j] = (dp[i-1][j] + (j > 0 ? dp[i][j-1] : 0)) % MOD;
+/*
+ * dp[k][n] : 정수 k개로 만들어 합 n이 되는 최대 경우의 수
+ * 중복을 허락
+ * dp[i][j] = dp[i-1][j] + dp[i][j-1]
+ * k-1개의 정수에 0을 더하면 합n의 k개로 만들 수 있다
+ * k개의 정수를 합 n-1로 한 것에 +1을 하면 n이 된다.
+ *
+ * */
+import java.io.*;
+import java.util.*;
+class Main {
+    public static void main(String[] agrs) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[k+1][n+1];
+        for(int i=1;i<=k; i++){
+            for(int j=0;j<=n;j++){
+                if(i==1 || j==0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) %  1000000000;
             }
         }
-
-        // 결과 출력
-        System.out.println(dp[K][N]);
+        System.out.println(dp[k][n]);
     }
 }
