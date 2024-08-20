78261330762ffbf6c55f09711a37ae27cf6d25d9
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-08-20 12:48:30 +0900

    [level 2] Title: 택배상자, Time: 59.47 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,53 +1,22 @@
-// import java.util.*;
-// class Solution {
-//     public int solution(int[] order) {
-//         int[] boxes = new int[order.length];
-//         for(int i=0;i<boxes.length;i++){
-//             boxes[order[i]-1] = i;
-//         }
-//         Stack<Integer> stack = new Stack<>();
-//         int num = 1;
-//         for(int box : boxes){
-//             if(num!= box){
-//                 stack.push(box);
-
-//             }else{
-                
-//                 num++;
-//             }
-//            while(!stack.isEmpty() && stack.peek()==num){
-
-//                 stack.pop();               
-//                 num++;
-//            }
-            
-//         }
-//         while(!stack.isEmpty() && stack.peek()==num){
-
-//                 stack.pop();               
-//                 num++;
-//            }
-        
-//         return num;
-//     }
-// }
 
 import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
         Stack<Integer> stack = new Stack<>();
-        int index = 0;  
-        
-        for (int i = 1; i <= order.length; i++) {
-            stack.push(i);
-            
-            while (!stack.isEmpty() && stack.peek() == order[index]) {
+        int[] boxes = new int[order.length];
+        for(int i=0;i<order.length;i++){
+            boxes[order[i]-1] = i;
+        }
+        int num = 0;
+        for(int box : boxes){
+            stack.push(box);
+            while(!stack.isEmpty() && stack.peek() == num){
                 stack.pop();
-                index++;
+                num++;
             }
         }
         
-        return index;
+        return num;
     }
 }
