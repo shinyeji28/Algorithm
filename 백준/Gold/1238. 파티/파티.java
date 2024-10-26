a34edd414bc2520cf436c01d8cec2582f4a381b5
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-10-26 15:21:26 +0900

    [Gold III] Title: 파티, Time: 764 ms, Memory: 64952 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,12 +1,7 @@
-/*
-* 다익스트라
-* 왕복 = 각 노드에서 목표지까지 최단 거리 + 목표지에서 본인 노드까지 최단 거리
-* 최대 비용이 걸리는 노드의 비용 출력
-* */
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main {
-    static class Node{
+    public static class Node{
         int to;
         int cost;
         public Node(int to, int cost){
@@ -14,21 +9,18 @@ public class Main {
             this.cost = cost;
         }
     }
-    static List<Node>[] map;
+    static List<Node>[] graph;
     static int n;
-    static int[] roundTrip;
-    static int[] distance;
-    public static void main(String[] agrs)throws IOException{
+    public static void main(String[] agrs) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
 
-        map = new List[n+1];
-        roundTrip = new int[n+1];
-        for(int i=1;i<=n;i++){
-            map[i] = new ArrayList<>();
+        graph = new ArrayList[n+1];
+        for(int i=0;i<n+1;i++){
+            graph[i] = new ArrayList<>();
         }
 
         for(int i=0;i<m;i++){
@@ -36,51 +28,51 @@ public class Main {
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            map[from].add(new Node(to,cost));
+
+            graph[from].add(new Node(to,cost));
         }
 
-        // 파티에 갈 최단 경로
+        int[] totalCost = new int[n+1];
         for(int i=1;i<=n;i++){
-
-            dijkstra(i,x);
+            int[] dis = bfs(i);
+            totalCost[i] += dis[x];
             if(i==x){
-                for(int j=1;j<=n;j++){
-                    roundTrip[j] += distance[j];
+                for(int j=1;j<n+1;j++){
+                    totalCost[j] += dis[j];
                 }
-                continue;
             }
-            roundTrip[i] += distance[x];
-
         }
+
         int result = 0;
         for(int i=1;i<=n;i++){
-            if(i!=x) result = Math.max(result, roundTrip[i]);
+            result = Math.max(result, totalCost[i]);
         }
-
         System.out.println(result);
     }
-    public static void dijkstra(int start, int x){
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(node->node.cost));
+    public static int[] bfs(int from){
+        PriorityQueue<Node> pq = new PriorityQueue<>((a,b)->a.cost-b.cost);
         boolean[] visited = new boolean[n+1];
-        distance = new int[n+1];
+        int[] distance = new int[n+1];
         Arrays.fill(distance, Integer.MAX_VALUE);
-        pq.offer(new Node(start, 0));
-        distance[start] = 0;
+
+        pq.offer(new Node(from, 0));
+        distance[from] = 0;
+
         while(!pq.isEmpty()){
             Node cur = pq.poll();
-            int node = cur.to;
-            int cost = cur.cost;
-            if(visited[node]) continue;
-            visited[node] = true;
-
-            for(Node nextNode: map[node]){
-                int nNode = nextNode.to;
-                int nCost = cost + nextNode.cost;
-                if(distance[nNode]>nCost) {
-                    distance[nNode] = nCost;
-                    pq.offer(new Node(nNode, nCost));
+            int to = cur.to;
+            if(visited[to])continue;
+            visited[to] = true;
+            for(Node next : graph[to]){
+                if(distance[next.to] > distance[to] + next.cost){
+                    distance[next.to] = distance[to] + next.cost;
+                    pq.offer(new Node (next.to, distance[next.to]));
                 }
+
             }
         }
+
+
+        return distance;
     }
 }
