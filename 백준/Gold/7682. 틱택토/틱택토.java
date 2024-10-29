6fe239513f113f2e9d4494cbe8bbe0ef103d32be
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-10-29 18:29:04 +0900

    [Gold V] Title: 틱택토, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.java
+++ 백준/Gold/7682. 틱택토/틱택토.java
@@ -1,63 +1,103 @@
-/*
-* 최종 상태 가능
-*  - 다 채워짐 (X = 5, O = 4)
-*  - X혹은 O가 가로, 세로, 대각선으로 빙고
-*
-* 불가능
-* - 입력에서 거르기 (O > X)
-*
-* 판단
-* i<3까지 돌면서
-* - 가로 : if(i == i+1 == i+2)
-* - 세로 : if(i*3 == i*3+1 == i*3+2)
-*
-* - 대각(좌상-우하) : if(0 == 4 == 8)
-* - 대각 (우상- 좌하) : if(2 == 4 == 6)
-* */
-import java.io.*;
 import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] agrs) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        char[] map = new char[9];
-        int x = 0;
-        int o = 0;
-        int[][] posCompare = new int[][]{
-                {0,3,6}, {1,4,7}, {2,5,8},   // 세로
-                {0,1,2}, {3,4,5}, {6,7,8},   // 가로
-                {0,4,8}, {2,4,6}             // 대각선
-        };
 
-        A : while(true) {
-            st = new StringTokenizer(br.readLine());
-            String input = st.nextToken();
-            if (input.equals("end")) break;
-            x = 0;
-            o = 0;
-            for (int i = 0; i < 9; i++) {
-                map[i] = input.charAt(i);
-                if (map[i] == 'X') x++;
-                if (map[i] == 'O') o++;
+    public static void main(String[] agrs) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while(true){
+            String input = br.readLine();
+            if(input.equals("end"))break;
+
+            char[] map = input.toCharArray();
+
+            int xCnt = 0;
+            int oCnt = 0;
+            for(int i=0;i<map.length;i++){
+                if(map[i] == 'X') xCnt++;
+                else if(map[i] == 'O') oCnt++;
             }
-            if (o > x || x > o+1) {
+
+            // !(x - 1 = o || x = o) -> invalid
+            if(!(xCnt - 1 == oCnt || xCnt == oCnt)) {
                 sb.append("invalid").append('\n');
                 continue;
             }
-            boolean oFlag = false, xFlag = false;
-            for (int[] pos : posCompare) {
-                if (map[pos[0]] != '.' && map[pos[0]] == map[pos[1]] && map[pos[1]] == map[pos[2]]) {
-                    if (map[pos[0]] == 'O') oFlag = true;
-                    else if(map[pos[0]]=='X') xFlag = true;
-                }
+
+            boolean[] winInfo = isWinSomeOne(map);
+
+            // 둘다 빙고
+            if(winInfo[0] && winInfo[1]){
+                sb.append("invalid").append('\n');
+                continue;
             }
-            if (oFlag && xFlag) sb.append("invalid").append('\n');
-            else if (oFlag && o==x) sb.append("valid").append('\n');
-            else if (xFlag && o+1==x) sb.append("valid").append('\n');
-            else if(!oFlag && !xFlag && x == 5 && o ==4) sb.append("valid").append('\n');
-            else sb.append("invalid").append('\n');
+
+            // 게임이 끝나지 않음
+            if((9-(xCnt + oCnt)) != 0 && !winInfo[0] && !winInfo[1]){
+                sb.append("invalid").append('\n');
+                continue;
+            }
+
+            // (9 -(x+o))==0 && x - 1 != o   -> invalid
+            if(((9-(xCnt + oCnt)) == 0) && xCnt - 1 != oCnt){
+                sb.append("invalid").append('\n');
+                continue;
+            }
+            // (3X && x - 1 != 0) || (3O && x != o) -> invalid
+            if((winInfo[0] && xCnt - 1 !=oCnt) || (winInfo[1] && xCnt!=oCnt) ){
+                sb.append("invalid").append('\n');
+                continue;
+            }
+            sb.append("valid").append('\n');
         }
         System.out.println(sb);
+    }
+    public static boolean[] isWinSomeOne(char[] map){
+
+        boolean x = false;
+        boolean o = false;
+
+        for(int i=0;i<3;i++){
+            // 행으로 win
+            char prev = '0';
+            int cnt = 0;
+            for(int j=i;j<9;j+=3){
+                if(j==i) prev = map[j];
+                if(map[j] != prev) break;
+                else{
+                    cnt++;
+                    if(cnt == 3){
+                        if(map[j] == 'X') x = true;
+                        if(map[j] == 'O') o = true;
+                    }
+                }
+            }
+
+            // 열로 win
+            cnt = 0;
+            for(int j=i*3;j<9;j++){
+                if(j==i*3) prev = map[j];
+                if(map[j] != prev) break;
+                else{
+                    cnt++;
+                    if(cnt == 3){
+                        if(map[j] == 'X') x = true;
+                        if(map[j] == 'O') o = true;
+                    }
+                }
+            }
+        }
+
+        if(map[0]==map[4] && map[4]==map[8]){
+            if(map[0] == 'X') x = true;
+            if(map[0] == 'O') o = true;
+        }
+        if(map[2]==map[4] && map[4]==map[6]){
+            if(map[2] == 'X') x = true;
+            if(map[2] == 'O') o = true;
+        }
+
+
+        return new boolean[]{x,o};
     }
 }
