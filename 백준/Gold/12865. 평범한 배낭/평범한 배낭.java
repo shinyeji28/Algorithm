6514e0ddfc144226e179c4ab323123d358c5f247
신예지 <71514775+shinyeji28@users.noreply.github.com>
2024-10-30 18:56:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 108 ms, Memory: 12124 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,51 +1,34 @@
-
-/*
- *  V(가치)의 최대값 구하기
- *
- * W(무게) <= K (배낭)
- *
- * 방법 >
- * 무게가 작은 것부터 넣거나 가치가 큰 것부터 넣으면 안됨
- *
- * 1) 완탐
- * 모든 경우를 다 고려해야 함
- * 시간 복잡도 : O(N^N) = 100^100 - 시간 초과
- * 2) DP
- * 아이디어 : 물건 하나를 담을 때, 1~k무게를 담는 가방의 최대 가치
- * dp[i] : i라는 무게를 담는 가방에 대한 최대의 가치
- * (w,v)의 물건을 가방에 들어갈 경우 k 무게의 가방에 들어갈 가치 = v + dp[k-w]
- * 시간 복잡도 : O(N*K) = 10,000,000
- *
- * */
-
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int bagWeight = Integer.parseInt(st.nextToken());
 
-        Queue<int[]> goods = new LinkedList<>();
-        for(int i=0;i<n;i++){
+        int[][] goods = new int[n+1][2];
+        for(int i=1;i<=n;i++){
             st = new StringTokenizer(br.readLine());
-            goods.offer(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            goods[i][0] = w;
+            goods[i][1] = v;
         }
 
-        int[] dp = new int[k+1];
 
-        // 물건을 하나씩 가방에 넣기
-        A : for(int i=0;i<n;i++){
-            int[] item = goods.poll();
-            int w = item[0];
-            int v = item[1];
-
-            for(int j=k;j>(j-w);j--){   // 임시 가방(무게가 1~k)에 해당 물건을 넣을 때 각 임시 가방의 최대 가치
-                if(w>j) continue A;   // 가방의 무게보다 물건이 무거우면 더 이상 탐색할 필요 없음
-                dp[j] = Math.max(dp[j], dp[j-w] + v);
+        int[] dp = new int[bagWeight+1];
+        // 현재 물건을 넣기 : 현재 물건의 가치 + dp[i-현재 물건의 w] : v + dp[i-1][j-w]
+        // 현재 물건 안 넣기 : dp[이전 물건까지의 가치]             : dp[i-1][j];
+        // 이 중 가장 큰 것
+        for(int i=1;i<=n;i++){
+            int w = goods[i][0];
+            int v = goods[i][1];
+            for(int j=bagWeight;j>=w;j--){
+                if(j-w>=0) dp[j] = Math.max(v + dp[j-w], dp[j]);
             }
         }
-        System.out.println(dp[k]);
+        System.out.println(dp[bagWeight]);
     }
 }
