70c004e692c735acbfc4dedde156db7c0f85457b
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-02-18 17:10:31 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 81.98 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=java) 
 
 ### 성능 요약
 
-메모리: 69 MB, 시간: 79.95 ms
+메모리: 74.3 MB, 시간: 81.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 04일 18:06:01
+2025년 02월 18일 17:10:27
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,59 +1,65 @@
+/*
+    1. 각 덩어리의 크기를 구함, 덩어리 내 구역의 임시 열에 덩어리 크기를 저장 (중복 방지를 위함)
+    2. 최종 열 += 임시 열
+    3. 최종 열 중 최대 값 출력
+*/
+
 import java.util.*;
 class Solution {
+    static boolean[][] visited;
+    static boolean[] isOil;  // 석유가 있는 열 위치에 체크 
     public int solution(int[][] land) {
         
+        int result = 0;
+        int rSize = land.length;
+        int cSize = land[0].length;
+        visited = new boolean[rSize][cSize];
+        
+        int[] countingOil = new int[cSize];
+        
+        for(int i=0;i<rSize;i++){
+            for(int j=0;j<cSize;j++){
+                if(land[i][j]==1 && !visited[i][j]){
+                    isOil = new boolean[cSize];
+                    int sum = detectOil(i,j, land);
+                    
+                    for(int k=0;k<cSize;k++){
+                        if(isOil[k])countingOil[k] += sum;
+                    }
+                }
+            }
+        }
+        
+        for(int i=0;i<cSize;i++){
+            result = Math.max(countingOil[i],result);
+        }
+        return result;
+    }
+    //bfs
+    public static int detectOil(int cx, int cy, int[][] land){
         int[] dx = new int[]{0,0,1,-1};
         int[] dy = new int[]{1,-1,0,0};
         
-        int[][] oil = new int[land.length][land[0].length]; 
-        HashMap<Integer, Integer> oilCost = new HashMap<>();
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{cx,cy});
+        visited[cx][cy] = true;
+        int sum = 0;
         
-        // 석유 값 구하기
-        boolean[][] visited = new boolean[land.length][land[0].length];
-        int idx = 0;
-
-        for(int i=0;i<land.length;i++){
-            for(int j=0;j<land[0].length;j++){
-                if(land[i][j] == 0 || visited[i][j])continue;
-                idx+=1;
-
-                Queue<int[]> q = new ArrayDeque<>();
-                q.offer(new int[]{i,j});
-                visited[i][j] = true;
-                int cnt = 0;
-                while(!q.isEmpty()){
-                    int[] cur = q.poll();
-                    int x = cur[0];
-                    int y = cur[1];
-                    cnt++;
-                    oil[x][y] = idx;
-                    for(int d=0;d<dx.length;d++){
-                        int nx = x + dx[d];
-                        int ny = y + dy[d];
-                        if(nx<0||ny<0||nx>=land.length||ny>=land[0].length||visited[nx][ny] || land[nx][ny]==0)continue;
-                        q.offer(new int[]{nx,ny});
-                        visited[nx][ny] = true;
-                    }
-                }
-                oilCost.put(idx,cnt);
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+            isOil[y] = true;
+            sum++;
+            for(int d=0;d<4;d++){
+                int nx = x + dx[d];
+                int ny = y + dy[d];
+                if(nx<0||ny<0||nx>=land.length||ny>=land[0].length|| visited[nx][ny]||land[nx][ny]==0)continue;
+                q.offer(new int[]{nx,ny});
+                visited[nx][ny] = true;
+                
             }
         }
-
-        // 시추관 파기
-        int totalMaxCost = 0; // 전체 가장 큰 수
-        for(int j=0;j<land[0].length;j++){
-            int sum = 0; // 한줄 합
-            HashSet<Integer> hs = new HashSet<>();         // 동일한 오일 구역을 시추했는지 확인
-            for(int i=0;i<land.length;i++){
-                if(oil[i][j] == 0)continue;
-                if(!hs.contains(oil[i][j])){
-                    sum += oilCost.get(oil[i][j]);
-                    hs.add(oil[i][j]);
-                }
-            }
-            totalMaxCost = Math.max(totalMaxCost, sum);
-        }
-        return totalMaxCost;
-
+        return sum;
     }
 }
