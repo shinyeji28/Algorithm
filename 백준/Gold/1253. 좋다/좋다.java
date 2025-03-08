a17af72744efe360948724a7208db34b92d76f5e
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-03-08 23:16:21 +0900

    [Gold IV] Title: 좋다, Time: 120 ms, Memory: 12820 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,43 +1,42 @@
-
-/*
-* N^2
-* */
 import java.util.*;
 import java.io.*;
-public class Main {
-
-    public static void main(String[] agrs)throws IOException {
+public class Main{
+    public static void main(String[] agrs) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int[] arr = new int[n];
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(st.nextToken());
-            arr[i] = input;
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
 
         for(int i=0;i<n;i++){
+            int target = arr[i];
+
+            // 투포인터로 target이 되는 두 수 찾기
             int left = 0;
             int right = n-1;
             while(left < right){
-                if(i==left)left++;
-                if(i==right)right--;
-                if(left == right)break;
+                if(left == i) {
+                    left++;
+                    continue;
+                }
+                if(right == i) {
+                    right--;
+                    continue;
+                }
                 int sum = arr[left] + arr[right];
-                if(arr[i] == sum){
-                    answer++;
+                if(target == sum){
+                    cnt++;
                     break;
                 }
-                if(arr[i] < sum){
-                    right--;
-                }else{
-                    left++;
-                }
+                if(target > sum) left++;
+                else right--;
             }
+
         }
-        System.out.println(answer);
+        System.out.println(cnt);
     }
 }
