7c41cd93594076e62c895c9d746377d31922bb45
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-03 19:33:49 +0900

    [Gold IV] Title: 수도배관공사, Time: 320 ms, Memory: 149116 KB -BaekjoonHub

---
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int D = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int[][] pipe = new int[P+1][2];
+        for(int i=1;i<=P;i++){
+            st = new StringTokenizer(br.readLine());
+            int l = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            pipe[i][0] = l;
+            pipe[i][1] = c;
+        }
+
+        int[][] dp = new int[P+1][D+1];
+        for(int i=0;i<=P;i++){
+            Arrays.fill(dp[i],-1);
+            dp[i][0] = Integer.MAX_VALUE;
+        }
+
+
+        for(int i=1;i<=P;i++){
+            int l = pipe[i][0];
+            int c = pipe[i][1];
+            for(int j=1;j<=D;j++){
+                // 파이프 사용 안하기
+                dp[i][j] = dp[i-1][j];
+
+                // 파이프 사용하기
+                if(j-l>=0 && dp[i-1][j-l]!=-1)dp[i][j] = Math.max(dp[i][j], Math.min(c, dp[i-1][j-l]));
+
+            }
+        }
+        System.out.println(dp[P][D]);
+
+
+    }
+}
