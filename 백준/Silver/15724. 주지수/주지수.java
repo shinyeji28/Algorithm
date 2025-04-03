40a760417229221d73f1c0463133e13ccfa9d386
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-03 22:40:47 +0900

    [Silver I] Title: 주지수, Time: 608 ms, Memory: 124756 KB -BaekjoonHub

--- 백준/Silver/15724. 주지수/주지수.java
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -2,23 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static int[][] map;
-    static int[][] sum;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        map = new int[N+1][M+1];
-        sum = new int[N+1][M+1];
+        int[][] map = new int[N+1][M+1];
+        int[][] dp = new int[N+1][M+1];
 
         for(int i=1;i<N+1;i++){
             st = new StringTokenizer(br.readLine());
             for(int j=1;j<M+1;j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-                sum[i][j] = map[i][j] + sum[i][j-1];
             }
         }
+
+        for(int i=1;i<N+1;i++){
+            for(int j=1;j<M+1;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + map[i][j];
+            }
+        }
+
+
         int K = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<K;i++){
@@ -27,18 +32,13 @@ public class Main{
             int sy = Integer.parseInt(st.nextToken());
             int ex = Integer.parseInt(st.nextToken());
             int ey = Integer.parseInt(st.nextToken());
-            sb.append(getSquareSum(sx,sy,ex,ey)).append('\n');
+
+            int result = dp[ex][ey] - dp[sx-1][ey] - dp[ex][sy-1] + dp[sx-1][sy-1];
+            sb.append(result).append('\n');
         }
 
         System.out.println(sb);
 
 
-    }
-    public static int getSquareSum(int sx,int sy,int ex,int ey){
-        int total = 0;
-        for(int i=sx;i<=ex;i++){
-            total += sum[i][ey] - sum[i][sy] + map[i][sy];
-        }
-        return total;
     }
 }
