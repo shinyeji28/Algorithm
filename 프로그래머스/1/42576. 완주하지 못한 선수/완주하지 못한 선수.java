09052d12060f357c7a14f4212fc823db85371496
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-05 15:25:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 325.27 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,21 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         
-        HashMap<String, Integer> completionHash = new HashMap<>();
+        String answer = ""; 
         
-        for(String c : completion){
-            completionHash.put(c, completionHash.getOrDefault(c, 0) + 1);
-        }
-        for(String p : participant){
-            if(completionHash.get(p)!=null && completionHash.get(p)!=0){
-                completionHash.put(p, completionHash.get(p)-1);
-            }else{
-                answer = p;
+        // 이름 정렬 후 선형탐색으로 이름이 다른다면 완주하기 못한 선수임 (완주 못한 선수는 1명이기 때문)
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        answer = participant[participant.length-1];
+
+        for(int i=0;i<completion.length;i++){
+            if(!participant[i].equals(completion[i])){
+                answer = participant[i];
                 break;
             }
-        }
-        
+        } 
         return answer;
     }
 }
