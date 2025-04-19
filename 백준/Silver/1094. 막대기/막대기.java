d8efc0967b9a2cb189c144abf72a80642c7249f0
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-19 21:00:53 +0900

    [Silver V] Title: 막대기, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,9 +1,26 @@
-import java.io.*;
-public class Main {
-    public static void main(String[] agrs) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int target = Integer.parseInt(br.readLine());
 
-        System.out.println(Integer.bitCount(target));
+    import java.io.*;
+    import java.util.*;
+    public class Main{
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+
+            int num = 64;
+            int cnt = 0;
+            int sum = 0;
+            if(n == num) cnt = 1;
+            else {
+                while (num != 1) {
+                    if (sum == n) break;
+                    num = num / 2;
+
+                    if (sum + num <= n) {
+                        sum += num;
+                        cnt++;
+                    }
+                }
+            }
+            System.out.println(cnt);
+        }
     }
-}
