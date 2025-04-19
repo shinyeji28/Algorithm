27022a59cbb067a2ece9d6c9e0cd6e9893997433
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-19 20:14:11 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 76 ms, Memory: 12036 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,64 +1,55 @@
-import java.util.*;
-import java.io.*;
-public class Main {
-    static int[][] map;
-    static int n,m;
-    public static void main(String[] agrs) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
 
-        map =  new int[n][m];
+    import java.io.*;
+    import java.util.*;
+    public class Main{
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
 
-        for(int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            for(int j=0;j<m;j++){
-                map[i][j] = (str.charAt(j))=='W'? 1:0;
+            boolean[][] board = new boolean[n][m];
+            for(int i=0;i<n;i++){
+                st = new StringTokenizer(br.readLine());
+                String str = st.nextToken();
+                for(int j=0;j<m;j++){
+                    char color = str.charAt(j);
+                    if(color == 'W') board[i][j] = true;
+                    else board[i][j] = false;
+                }
             }
-        }
 
-        int result = Integer.MAX_VALUE;
-
-        for(int i=0;i<=n-8;i++){
-            for(int j=0;j<=m-8;j++){
-
-                result = Math.min(result, bfs(i,j, 0));
-                result = Math.min(result, bfs(i,j, 1));
+            // 정의에 따른 보드판 만들기
+            boolean[][] collectBoard = new boolean[n][m];
+            collectBoard[0][0] = true;
+            for(int j=1;j<m;j++) {
+                collectBoard[0][j] = !collectBoard[0][j-1];
             }
+            for(int i=1;i<n;i++){
+                for(int j=0;j<m;j++) {
+                    collectBoard[i][j] = !collectBoard[i-1][j];
+                }
+            }
+
+            int minValue = Integer.MAX_VALUE;
+
+            // 다시 칠해야 하는 갯수 찾기
+            for(int sx = 0;sx <=n-8;sx++){
+                for(int sy =0;sy<=m-8;sy++){
+
+                    int startWhite = 0;
+                    int startBlack = 0;
+                    for(int i=sx;i<sx + 8;i++) {
+                        for (int j = sy; j < sy +8; j++) {
+                            if(board[i][j] == collectBoard[i][j]) startWhite++;
+                            if(board[i][j] != collectBoard[i][j]) startBlack++;
+                        }
+                    }
+                    minValue = Math.min(minValue , Math.min(startWhite,startBlack));
+
+                }
+            }
+            System.out.println(minValue);
+
         }
-        System.out.println(result);
     }
-    public static int bfs(int x, int y, int first){
-
-        int[] dx = new int[]{0,1};
-        int[] dy = new int[]{1,0};
-
-        int cnt = 0;
-        Queue<int[]> q = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][m];
-
-        q.offer(new int[]{x,y, first});
-        visited[x][y] = true;
-        if(first != map[x][y])cnt++;
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-            int value = cur[2];
-            for(int d=0;d<dx.length;d++){
-                int nx = cx + dx[d];
-                int ny = cy + dy[d];
-                if(nx<0||ny<0||nx>=x + 8||ny>=y + 8||visited[nx][ny])continue;
-                int nextValue = value==0?1:0;
-                q.offer(new int[]{nx,ny,nextValue});
-                visited[nx][ny] = true;
-                if(map[nx][ny]!= nextValue) cnt++;
-            }
-        }
-
-        return cnt;
-    }
-}
