3b09fc2d1452f8772618e1b13de1afbb4be61798
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-19 18:13:32 +0900

    [Gold V] Title: 동전 2, Time: 88 ms, Memory: 12168 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,26 +1,25 @@
-import java.util.*;
-import java.io.*;
-public class Main {
-    public static void main(String[] agrs)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] coins = new int[n];
-        for(int i=0;i<n;i++){
-            coins[i] = Integer.parseInt(br.readLine());
-        }
 
-        int[] dp = new int[k+1];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-
-        dp[0] = 0;
-
-        for(int i=1;i<dp.length;i++){
-            for(int coin: coins){
-                if(i-coin>=0 && dp[i-coin]!=Integer.MAX_VALUE) dp[i] = Math.min(dp[i-coin]+1, dp[i]);
+    import java.io.*;
+    import java.util.*;
+    public class Main{
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[] coins = new int[n];
+            int[] dp = new int[k+1];
+            Arrays.fill(dp,Integer.MAX_VALUE);
+            dp[0] = 0;
+            for(int i=0;i<n;i++){
+                coins[i] = Integer.parseInt(br.readLine());
             }
+            for(int i=1;i<k+1;i++){
+                for(int coin: coins){
+                    if(i-coin >= 0 && dp[i-coin]!=Integer.MAX_VALUE) dp[i] = Math.min(dp[i],dp[i-coin] + 1);
+                }
+            }
+            if(dp[k] == Integer.MAX_VALUE)System.out.println("-1");
+            else System.out.println(dp[k]);
         }
-        System.out.println(dp[k]==Integer.MAX_VALUE?-1:dp[k]);
     }
-}
