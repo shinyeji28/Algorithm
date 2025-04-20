3563d41249d76d239f25ed796ea43cfa13ecaefa
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-20 18:21:07 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 92 ms, Memory: 12040 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,24 +1,32 @@
-import java.util.*;
-import java.io.*;
-public class Main {
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n+1];
-        int maxLength = 1;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,1);
-        for(int i=0;i<n;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-            for(int j=0;j<i;j++){
-                if(arr[i]>arr[j]){
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+
+    import java.io.*;
+    import java.util.*;
+
+    public class Main{
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int i=0;i<n;i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            int[] dp = new int[n];
+            Arrays.fill(dp, 1);
+            for(int i=1;i<n;i++){
+                for(int j=i-1;j>=0;j--){
+                    if(arr[i] > arr[j]){
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }else if(arr[i] == arr[j]){
+                        dp[i] = Math.max(dp[i], dp[j]);
+                    }
                 }
             }
-            maxLength = Math.max(maxLength,dp[i]);
+            int maxValue = 0;
+            for(int i=0;i<n;i++){
+                maxValue = Math.max(dp[i], maxValue);
+            }
+            System.out.println(maxValue);
         }
-        System.out.println(maxLength);
     }
-}
