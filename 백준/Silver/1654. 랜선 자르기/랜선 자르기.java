b1bdc030aca752581771e64c7693fe7002810ba6
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-20 19:50:39 +0900

    [Silver II] Title: 랜선 자르기, Time: 116 ms, Memory: 15440 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,38 +1,39 @@
-import java.util.*;
-import java.io.*;
-public class Main{
-    public static void main(String[] agrs) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        long[] myLen = new long[k];
-        for(int i=0;i<k;i++){
-            myLen[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(myLen);
 
-        long maxLen = 0;
-        long left = 1;
-        long right = myLen[k-1];
-        while(left <= right){
-            long mid = (left + right)/2;
+    import java.io.*;
+    import java.util.*;
 
-            long cnt = 0;
-            for(int i=k-1;i>=0;i--){
-                cnt += myLen[i] / mid;
+    public class Main{
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            long n = Long.parseLong(st.nextToken());
+            long[] arr = new long[k];
+
+            long left = 1;
+            long right = 0;
+            for(int i=0;i<k;i++){
+                arr[i] = Long.parseLong(br.readLine());
+                right = Math.max(right, arr[i]);
+            }
+
+            long answer = 0;
+            while(left <= right){
+                long mid = (left + right) / 2;
+
+                long cnt = 0;
+                for(int i=0;i<k;i++){
+                    cnt += arr[i] / mid;
+                }
+
                 if(cnt >= n){
-                    maxLen = Math.max(maxLen, mid);
-                    break;
+                    left = mid + 1;
+                    answer = Math.max(answer, mid);
+                }else {
+                    right = mid - 1;
                 }
             }
-
-            if(mid <= maxLen){
-                left = mid + 1;
-            }else{
-                right = mid - 1;
-            }
+            System.out.println(answer);
         }
-        System.out.println(maxLen);
     }
-}
