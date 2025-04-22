e405291dbc22bba320536a93081ee77589cb0ac0
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-22 16:57:48 +0900

    [level 3] Title: 입국심사, Time: 45.80 ms, Memory: 98.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 80.67 ms
+메모리: 98.2 MB, 시간: 45.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 15:07:37
+2025년 04월 22일 16:57:50
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,36 +1,23 @@
-/*
-    아이디어 : 1~1000000000에서 최소 시간 찾기 
-    이분탐색으로 각 시간에서 모든 사람이 심사를 받을 수 있는지 판단하여 
-        심사 가능 -> 더 작은 시간으로 찾기
-        심사 불가능 -> 더 큰 시간으로 찾기
-*/
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
         
-        long left = 1;
-        long right = 1000000000L * 1000000000L;
+        long left = 1L;
+        long right = 1000000000L * 100000L;
         long answer = right;
-
-        long mid = 0;
         while(left <= right){
-            mid = left + (right - left) / 2L;
-            
-            // 심사 가능 여부 판단
-            long num = 0;
-            for(int i=0;i<times.length;i++){
-                num += mid / times[i];
-                if(num >= n)break;
+            long mid = (left + right) / 2L;
+            long complete = 0L;
+            for(int t : times){
+                complete += mid / (long)t;
             }
-            if(num >= n){
-                answer = mid;
-                right = mid - 1;
-            }else{
-                left = mid + 1;
+            if(complete < n){
+                left = mid + 1L;
+            }else {
+                right = mid - 1L;
+                answer = Math.min(answer, mid);
             }
         }
-        
         return answer;
     }
 }
