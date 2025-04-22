e5a1aeda2504e4aa72e60414b4e570cd2a8c4fb1
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-04-22 16:15:11 +0900

    [level 4] Title: 징검다리, Time: 48.29 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/README.md
+++ 프로그래머스/4/43236. 징검다리/README.md
@@ -1,10 +1,10 @@
 # [level 4] 징검다리 - 43236 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43236) 
 
 ### 성능 요약
 
-메모리: 113 MB, 시간: 47.70 ms
+메모리: 82.3 MB, 시간: 48.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 24일 15:59:23
+2025년 04월 22일 16:15:13
 
 ### 문제 설명
 
--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,52 +1,38 @@
-/*
-    아이디어 : 거리의 최소값을 이분탐색으로 찾아 가능 여부 탐색
-    mid가 최소값이 되기 위해서는 거리의 차가 mid보다 모두 커야함
-    mid보다 거리의차가 작으면 바위 제거
-    n개만큼 바위를 제거했는데도 mid보다 작은 것이 있다면 mid를 늘려야함
-*/
 import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
-        int answer = Integer.MIN_VALUE;
+        int answer = 0;
+        
         Arrays.sort(rocks);
         
         int left = 0;
-        int right = distance;
-        while(left<=right){
-            int mid = left + (right - left)/2;
-            // 판단
-            if(isValid(rocks, mid, n, distance)){
-                answer = Math.max(mid, answer);
-                left = mid + 1;
-
-            }else{
-                right = mid - 1;
+        int right = 1000000000;
+        while(left <= right){
+            int mid = (left + right) / 2; // 거리의 최소값
+            // mid가 가능한지 판단
+            int p = 0;
+            int removeCnt = 0;
+            for(int i=0;i<rocks.length;i++){
+                // mid가 사이값이 크면 돌 제거
+                int diff = rocks[i] - p;
+                if(mid > diff){
+                    removeCnt++;
+                }else{
+                    p = rocks[i];
+                }
+            }
+            if(mid > distance - p){
+                removeCnt++;
             }
             
+            if(removeCnt > n) { // 제거한 돌이 n을 넘으면 더 작은 수 탐색
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+                answer = Math.max(answer, mid);
+            }
         }
         
         return answer;
-    }
-    public static boolean isValid(int[] rocks, int mid, int n, int distance){
-        int prev = 0;
-        for(int r : rocks){
-            
-            int diff = r - prev;
-        
-            if(diff < mid){
-                if(n == 0) return false;
-                n--;
-            }else {
-                prev = r;
-            }
-
-        }
-        // 마지막 바위와 도작지점 사이 측정
-        int diff = distance - prev;
-        if(diff < mid){
-            if(n == 0) return false;
-            n--;
-        }
-        return true;
     }
 }
