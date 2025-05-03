27eff4b8a3110ea16e042872991aaa310bae8284
신예지 <71514775+shinyeji28@users.noreply.github.com>
2025-05-03 18:35:07 +0900

    [Bronze I] Title: 달팽이2, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Bronze/1952. 달팽이2/달팽이2.java
+++ 백준/Bronze/1952. 달팽이2/달팽이2.java
@@ -7,31 +7,23 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        // 달팽이 시계방향
-        int[] dx = new int[]{0,1,0,-1};
-        int[] dy = new int[]{1,0,-1,0};
-
-        boolean[] visited = new boolean[n*m];
-        int end = n*m;
-        int x = 0;
-        int y = 0;
-        int d = 0;
+        // 달팽이 시계방향 (수학)
+        int row = n-1;
+        int col = m;
+        int sum = 0;
         int answer = 0;
-        visited[0] = true;
-        end--;
-        while(end > 0){
-            int nx = x + dx[d];
-            int ny = y + dy[d];
-            if(nx<0||ny<0||nx>=n||ny>=m||visited[nx * m + ny]){
-                d = (d+1)%4;
-                answer++;
-                continue;
-            }
-            visited[nx * m + ny] = true;
-            x = nx;
-            y = ny;
-            end--;
+        while(sum < n*m){
+            sum += col;
+            col -= 1;
+            answer++;
+            if(sum == n*m) break;
+
+            sum += row;
+            row -= 1;
+            answer++;
+
         }
-        System.out.println(answer);
+
+        System.out.println(answer-1);
     }
 }
